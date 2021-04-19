cea171e9aae8da0a71645ad477e34e418c165b6d
beniwali-raja <82509019+beniwali-raja@users.noreply.github.com>
2021-04-20 03:24:03 +0530

    Continously download a file in loop

---
+++ download.py
@@ -0,0 +1,18 @@
+import wget, os
+
+print('Beginning file download with wget module')
+location = os.getcwd()
+no = 1
+name = '10GB.bin'
+url = 'https://speed.hetzner.de/10GB.bin'
+
+
+print("The file is downloading in "+location)
+while no:
+    print(f"Downloading file number {no}")
+    wget.download(url, location)
+    print(f"download number {no} completed. Now deleting")
+    path = os.path.join(location, name)
+    os.remove(path)
+    print("Deleted successfully")
+    no+=1
